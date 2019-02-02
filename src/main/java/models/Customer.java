58640cce37545a8dbcd666ea73d07024db5f2f82
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-02-02 10:46:11 +0100

    adding new features

---
+++ src/main/java/models/Customer.java
@@ -0,0 +1,22 @@
+package models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Customer {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String fullName;
+}

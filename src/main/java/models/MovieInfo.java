fa10f22a4c59623ec6635223875b2836014a087f
Grzegorz Chrzaszczyk <chrzaszczyk@gmail.com>
2019-02-03 10:23:07 +0100

    Cascade.PERSIST and orphan removal tested

--- src/main/java/models/MovieInfo.java
+++ src/main/java/models/MovieInfo.java
@@ -5,10 +5,7 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.List;
 
 @Data
@@ -25,9 +22,9 @@ public class MovieInfo {
     private String title;
     private Double avgScore;
 
-    @OneToMany(orphanRemoval = true, mappedBy = "movieInfo")
+    @OneToMany(mappedBy = "movieInfo", orphanRemoval = true, cascade = {CascadeType.PERSIST})
     private List<MovieCopy> copies;
 
-    @OneToMany(orphanRemoval = true, mappedBy = "movieInfo")
+    @OneToMany(mappedBy = "movieInfo", orphanRemoval = true, cascade = {CascadeType.PERSIST})
     private List<Rank> ranks;
 }
